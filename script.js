e58503eb09bc14c996caa8ea91340b48e6d924eb
Antoinebousquet <moodlinkproject@gmail.com>
2019-11-18 23:39:33 +0100

    second commit

--- script.js
+++ script.js
@@ -37,52 +37,125 @@
 
 
 // // Fonctionnalité 3 :
-// var firstButton = document.getElementsByClassName("btn-outline-secondary")[0];
-// console.log(firstButton)
+// var firstCard = document.getElementsByClassName("col-md-4")[0];
+// var firstCardButton = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[0];
 
-// var editButton = function() {
-// 	this.style.color = 'red';
+// var onFirstCardButtonClick = function() {
+// 	firstCard.style.color = "red";
 // };
 
-// firstButton.addEventListener("click", editButton);
+// firstCardButton.addEventListener("click", onFirstCardButtonClick);
 
 // // Fonctionnalité 4 :
-// var secondButton = document.getElementsByClassName("btn-outline-secondary")[1];
-// console.log(secondButton)
+// var secondCard = document.getElementsByClassName("col-md-4")[1];
+// var secondCardButton = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1];
 
-// var editButton2 = function() {
-// 	if (this.style.color === 'green') {
-// 		this.style.color = '';
+// var onSecondCardButtonClick = function() {
+// 	if (secondCard.style.color === 'green') {
+// 		secondCard.style.color = '' ;
 // 	}
-// 	else {
-// 		this.style.color = 'green';
+// 	else if (secondCard.style.color === '') {
+// 		secondCard.style.color = 'green'
 // 	};
 // };
 
-// secondButton.addEventListener("click", editButton2);
+// secondCardButton.addEventListener("click", onSecondCardButtonClick);
 
-// Fonctionnalité 5 :
-var nuclearButton = document.getElementsByClassName("container")[0];
-var linkCss = document.getElementsByTagName("link")[0];
-console.log(nuclearButton);
+// // Fonctionnalité 5 :
+// var nuclearButton = document.getElementsByClassName("navbar navbar-dark bg-dark box-shadow")[0];
+// var wholePage = document
+// var linkCss = document.getElementsByTagName("link")[0];
+// console.log(nuclearButton);
 
-var touchNavbar = function() {
-	linkCss.href = '#';
-};
+// var touchNavbar = function() {
+// 	if (linkCss.href === 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css') {
+// 		linkCss.href = '#';
+// 	}
+// 	else {
+// 		linkCss.href = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
+// 	};
+// };
 
-nuclearButton.addEventListener("click", touchNavbar);
-
-
-
-// Pour le fun, on va implémenter une fonctionnalité à la sauce ☢"nucléaire". 
-// Et comme elle est un peu dangereuse, on va la cacher… Voici comment elle doit marcher : 
-// si un utilisateur double clique sur la navbar en haut, tout Bootstrap disparaît et
-//  la page s'affiche comme si on avait oublié de mettre le CDN qui la relie au fichier CSS.
-//  Si possible, rends cette fonctionnalité réversible (un nouveau double-clic fait tout revenir à la normale).
-
+// nuclearButton.addEventListener("dblclick", touchNavbar);
+// // addEventListener("dblclick", touchNavbar);
 
 
+// // Fonctionnalité 5, méthode alternative :
+// var navbarClass = document.getElementsByTagName("header")[0];
+// var bootstrapLink = document.getElementsByTagName("link")[0];
+// var onNavbarClassClick = function() {
 
+// 	if (bootstrapLink.disabled == false){
+// 		bootstrapLink.disabled = true
+// 	}
+
+// 	else if (bootstrapLink.disabled == true){
+// 		bootstrapLink.disabled = false
+// 	}
+// };
+
+// navbarClass.addEventListener("click", onNavbarClassClick);
+
+// Fonctionnalité 6 :
+// var oneCard = document.getElementsByClassName("col-md-4")[0];
+// var oneCardViewButton = document.getElementsByClassName("btn btn-sm btn-success")[0];
+// var oneCardText = document.getElementsByClassName("card-text")[0];
+// var oneCardImage = document.getElementsByClassName("card-img-top")[0];
+// var onOneCardViewButtonMouseOver = function() {
+// 	if (oneCardText.className == "card-text"){
+// 		oneCardText.className = "card-text collapse" ;
+// 		oneCardImage.style.width = '20%'
+// 	}
+// 	else if (oneCardText.className == "card-text collapse"){
+// 		oneCardText.className = "card-text" ;
+// 		oneCardImage.className = "card-img-top"
+// 		oneCardImage.style.width = '100%'
+// 	};
+// };
+
+// oneCardViewButton.addEventListener("mouseover", onOneCardViewButtonMouseOver);
+
+
+
+var oneCard = document.getElementsByClassName("col-md-4")[0];
+var oneCardViewButton = document.getElementsByClassName("btn btn-sm btn-success");
+var oneCardText = document.getElementsByClassName("card-text")[0];
+var oneCardImage = document.getElementsByClassName("card-img-top")[0];
+// var onOneCardViewButtonMouseOver = function() {
+// 	if (oneCardText.className == "card-text"){
+// 		oneCardText.className = "card-text collapse" ;
+// 		oneCardImage.style.width = '20%'
+// 	}
+// 	else if (oneCardText.className == "card-text collapse"){
+// 		oneCardText.className = "card-text" ;
+// 		oneCardImage.className = "card-img-top"
+// 		oneCardImage.style.width = '100%'
+// 	};
+// };
+
+
+	for (let i = 0; i < oneCardViewButton.length; i++) {
+		if (oneCardViewButton[i].addEventListener("mouseover", onOneCardViewButtonMouseOver) == true) {
+		oneCardText[i].className = "card-text collapse" ;
+		oneCardImage[i].style.width = '20%'
+		}
+		
+	};
+
+
+
+
+// var cardText = document.getElementsByClassName("card-body");
+// var cardButton = document.getElementsByClassName("btn btn-sm btn-outline-secondary");
+
+// var onCardCollapse = function(cardText) {
+// 	for(let index in cardText) {
+// 		console.log("test");
+// 		cardText.className = "";
+// 	};
+// };
+
+// cardButton[0].addEventListener("click", onCardCollapse(cardText));
 
 
 
